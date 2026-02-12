b02ad85347c8c7f1c4bda5fb9dcf5b4c78962360
Damaris Azucena Pinzon Castillo <damarispinzoncastillo@gmail.com>
2026-02-12 17:17:44 -0600

    Laboratorio

---
+++ damaris/lab 4/L4_PINZON CASTILLO DAMARIS AZUCENA_1188926/L4_PINZON CASTILLO DAMARIS AZUCENA_1188926/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace L4_PINZON_CASTILLO_DAMARIS_AZUCENA_1188926
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entrada de usuario
+            Console.WriteLine("¿Como te llamas? ");
+            string nombre;
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola," +nombre+ " Bienvenido a C#");
+            //Salida de datos
+            
+            Console.WriteLine("Nombre del personaje ");
+            string name;
+            name = Console.ReadLine();
+
+            Console.WriteLine("Nivel del personaje ");
+            int entero = 32; //Numero entero
+
+            Console.WriteLine("El nombre del personaje es " + name);
+            Console.WriteLine("El nivel del personaje es " + entero);
+
+
+
+
+
+        }
+    }
+}
---
+++ damaris/lab 4/L4_PINZON CASTILLO DAMARIS AZUCENA_1188926/L4_PINZON CASTILLO DAMARIS AZUCENA_1188926/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("L4_PINZON CASTILLO DAMARIS AZUCENA_1188926")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("L4_PINZON CASTILLO DAMARIS AZUCENA_1188926")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("a0a191f4-127d-41e0-9a8a-5d162ad3091e")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
